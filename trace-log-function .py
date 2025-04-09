415cc706f9643050ac4100c3283935bdce1f75f1
phucuongds <77599023+phucuong13029x@users.noreply.github.com>
2025-04-09 09:40:35 +0700

    Update trace-log-function .py

--- trace-log-function .py
+++ trace-log-function .py
@@ -1,27 +1,27 @@
 import time
 import sys
 
-call_id      = 10000000
-call_stack   = {}
-folder_trace = '/home/cuong.huynh'
+
+sys_call_stack   = {}
+sys_folder_trace = os.path.join(pathlib.Path.cwd(), 'app')
 
 def trace_events(frame, event, arg):
-    global call_id
     func_n = frame.f_code.co_name
     func_f = frame.f_code.co_filename
     func_l = frame.f_code.co_firstlineno
-    if folder_trace not in func_f: return trace_events
+    if sys_folder_trace not in func_f: return trace_events
     if event == "call":
-        call_id += 1
-        call_stack[frame] = (call_id, time.time())
-        print(f"[ID: {call_id}] CALL: {func_n}() in {func_f}:{func_l}")
+        unique_id = f"{time.time():.6f}"
+        sys_call_stack[frame] = (unique_id, time.time())
+        debuglogs.debug(f"[ID: {unique_id}] Call: {func_n}() in {func_f}:{func_l}")
     elif event == "return":
-        if frame in call_stack:
-            unique_id, start_time = call_stack.pop(frame)
+        if frame in sys_call_stack:
+            unique_id, start_time = sys_call_stack.pop(frame)
             duration = time.time() - start_time
-            print(f"[ID: {unique_id}] RETURN: {func_n}() executed in {duration:.6f} sec")
+            debuglogs.debug(f"[ID: {unique_id}] Return: {func_n}() executed in {duration:.6f} sec")
     elif event == "exception":
-        print(f"EXCEPTION in {func_n}() in {func_f}:{func_l}")
+        exc_type, exc_value, _ = arg
+        debuglogs.debug(f"[EXCEPTION] {func_n}() in {func_f}:{func_l} -> {exc_type.__name__}: {exc_value}")
     return trace_events
 
-sys.setprofile(trace_events)
+sys.setprofile(trace_events.trace_events)
